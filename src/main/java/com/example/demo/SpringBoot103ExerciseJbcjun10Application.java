a1857a6ac519c183c94e2f646ff9e15072b4af15
kellel14 <kellel14@gmail.com>
2019-07-15 10:06:14 -0400

    first commit

---
+++ src/main/java/com/example/demo/SpringBoot103ExerciseJbcjun10Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot103ExerciseJbcjun10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot103ExerciseJbcjun10Application.class, args);
+	}
+
+}
